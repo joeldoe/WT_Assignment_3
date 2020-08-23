d6507e8ff3ef05911ec7d25460464c0f56a103ab
joeldoe <67823410+joeldoe@users.noreply.github.com>
2020-08-23 18:53:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+var url = "https://davids-restaurant.herokuapp.com/menu_items.json";
+var options = [];
+
+$(document).ready(function()
+{
+	$("#error").hide();
+
+	// GET method is called to retrieve the JSON data and to store its names into an array
+	$.get(url, function(data, status)
+	{
+		console.log(status);
+		
+		for(option of data.menu_items)
+		{
+			options.push(option.name);
+		}
+		createMenu();
+	});
+
+	$("#get-details").click(function()
+	{
+		if($("#list").val() == "Select an item")
+		{
+			$("#error").show();
+		}
+		else
+		{
+			$.get(url, function(data)
+			{
+				$("#error").hide();
+
+				for(option of data.menu_items)
+				{
+					if($("#list").val() == option.name)
+					{
+						$("#food-short-name").html("<b>Short name:</b> " + option.short_name);
+						$("#food-name").html("<b>Name:</b> " + option.name);
+						$("#data").html("<b>About the item:</b> " + option.description);
+				
+						if(option.price_small == null)
+						{
+							$("#max-price").html("<b>Price(large):</b> " + option.price_large);
+						}
+						else
+						{
+							$("#min-price").html("<b>Price(small):</b> " + option.price_small);
+							$("#max-price").html("<b>Price(large):</b> " + option.price_large);
+						}
+					}
+				}
+			});
+		}
+	});
+});
+
+// createMenu() will create the dropdown list of the food items
+function createMenu()
+{
+	let dropdown = document.getElementById('list');
+
+	options.forEach(function(foodItem)
+	{
+		let op = document.createElement('option');
+		op.textContent = foodItem;
+		dropdown.appendChild(op);
+	});
+}
